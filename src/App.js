eb98165311d41a13ed9f6b486442be478d95bfa8
Khadka <khadkarachandra981@gmil.com>
2024-08-05 21:02:01 +0545

    bmi calculation

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState(0);
+  const [message, setMessage] = useState("");
+
+  // Calculate BMI
+  const calcBmi = (e) => {
+    e.preventDefault();
+    if (weight === 0 || height === 0) {
+      alert("Please enter a valid weight and height");
+    } else {
+      let heightInMeters = height * 0.3048;
+      let bmi = weight / (heightInMeters * heightInMeters);
+      setBmi(bmi.toFixed(1));
+
+      if (bmi < 18.5) {
+        setMessage("You are underweight");
+      } else if (bmi >= 18.5 && bmi < 24.9) {
+        setMessage("You are healthy");
+      } else {
+        setMessage("You are overweight");
+      }
+    }
+  };
+
+  // Reload the page
+  const reload = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h2>BMI Calculator</h2>
+        <form onSubmit={calcBmi}>
+          <div>
+            <label>Weight (kg)</label>
+            <input
+              type="text"
+              placeholder="Enter Weight Value"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Height (feet)</label>
+            <input
+              type="text"
+              placeholder="Enter Height Value"
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+            />
+          </div>
+          <div>
+            <button className="btn" type="submit">
+              Submit
+            </button>
+            <button className="btn btn-outline" onClick={reload} type="button">
+              Reload
+            </button>
+          </div>
+          <div className="center">
+            <h3>
+              <b>Your BMI is: {bmi} </b>
+            </h3>
+            <p>{message}</p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
